ecbf4e0488d17716dc7df75a815ada110afb3a03
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2024-12-20 20:26:05 +0530

    commit nav

---
+++ 02React/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+
+  return (
+    <nav className="container">
+        <div>
+            <img src="/images/logo.png" alt="do some coding"/>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
